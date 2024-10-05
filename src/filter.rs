05d33e80569bb286c5e2c8606c243e900155a6fd
sam <sjackson864@outlook.com>
2024-10-05 11:46:43 +0200

    simplified filter

--- src/filter.rs
+++ src/filter.rs
@@ -4,18 +4,8 @@ use crate::sampler::Sampler;
 pub fn tent_filter(sampler: &mut Sampler) -> (f64, f64) {
     let (r1, r2) = sampler.next_2d();
 
-    let dx = if r1 < 0.5 {
-        (2. - 2. * r1).sqrt() - 1.
-    } else {
-        1. - (2. - 2. * r1).sqrt()
-    };
-
-    let dy = if r2 < 0.5 {
-        (2. - 2. * r2).sqrt() - 1.
-    } else {
-        1. - (2. - 2. * r2).sqrt()
-    };
+    let dx = if r1 < 0.5 { -1.0 } else { 1.0 } * (2.0 - 2.0 * r1).sqrt();
+    let dy = if r2 < 0.5 { -1.0 } else { 1.0 } * (2.0 - 2.0 * r2).sqrt();
 
     (dx, dy)
-
 }
